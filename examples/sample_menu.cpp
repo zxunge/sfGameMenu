82dc7dbf7026fb3e4cd72104e30fb74819a50fac
zxunge <3396563372@qq.com>
2025-11-02 11:44:03 +0800

    Refactor to support SFML 3 and reformat code.

--- examples/sample_menu.cpp
+++ examples/sample_menu.cpp
@@ -1,71 +1,67 @@
 #include "game_menu/game_menu.h"
 
 #include <SFML/Graphics.hpp>
+
 #include <memory>
 #include <vector>
 
-int main() {
-	sf::RenderWindow w( sf::VideoMode( 800, 600 ), "Sample Title", sf::Style::Close);
-	
-	sf::Font font;
-	font.loadFromFile( "sansation.ttf" );
-	game_menu::Style style {
-		.TitleFont = &font,
-		.ItemFont = &font,
-		.TitleFontSize = 36,
-		.ItemFontSize = 24,
-		.MenuTitleScaleFactor = 1,
-		.MenuItemScaleFactor = 1.5,
-		.colorScheme = {
-			.titleColor = 0xFFFFFF,
-			.itemColor = 0xFFFFFF,
-			.selectedColor = 0xFF22F1
-		},
-		.PaddingTitle = {
-			.top = 100,
-			.left = 0,
-		},
-		.PaddingItems = {
-			.top = 40,
-		},
-		.TitleAlign = game_menu::Align::Center,
-		.ItemAlign = game_menu::Align::Center
-	};
+int main()
+{
+    sf::RenderWindow w(sf::VideoMode(800, 600), "Sample Title", sf::Style::Close);
 
-	bool is_exit_requested = false;
+    sf::Font font;
+    font.loadFromFile("sansation.ttf");
+    game_menu::Style style{.TitleFont            = &font,
+                           .ItemFont             = &font,
+                           .TitleFontSize        = 36,
+                           .ItemFontSize         = 24,
+                           .MenuTitleScaleFactor = 1,
+                           .MenuItemScaleFactor  = 1.5,
+                           .colorScheme = {.titleColor = 0xFFFFFF, .itemColor = 0xFFFFFF, .selectedColor = 0xFF22F1},
+                           .PaddingTitle =
+                               {
+                                   .top  = 100,
+                                   .left = 0,
+                               },
+                           .PaddingItems =
+                               {
+                                   .top = 40,
+                               },
+                           .TitleAlign = game_menu::Align::Center,
+                           .ItemAlign  = game_menu::Align::Center};
 
-	std::vector<game_menu::MenuItem> items {
-		{ "New Game", [](sf::RenderTarget &target) {}},
-		{ "Load Game", [](sf::RenderTarget &target) {}},
-		{ "Leaderboard", [](sf::RenderTarget &target) {}},
-		{ "Settings", [](sf::RenderTarget &target) {}},
-		{ "Exit", [&is_exit_requested](sf::RenderTarget &target) {is_exit_requested = true;}}
-	};
+    bool is_exit_requested = false;
 
-	game_menu::MenuConfig config {
-		.title = "My Game",
-		.items = items,
-		.style = style
-	};
-	
-	auto menu_ptr = create_menu_context(w, config);
-	std::unique_ptr<game_menu::MENU, decltype(&menu_destroy_context)> menu(menu_ptr, &menu_destroy_context);
-	while (w.isOpen()) {
-		if (is_exit_requested) {
-			w.close();
-			break;
-		}
-		sf::Event event;
-		while (w.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				is_exit_requested = true;
-			}
-			menu_handle_event(menu.get(), event);
-		}
-		w.clear();
-		menu_render(menu.get());
-		w.display();
-	}
-	return 0;
+    std::vector<game_menu::MenuItem>
+        items{{"New Game", [](sf::RenderTarget& target) {}},
+              {"Load Game", [](sf::RenderTarget& target) {}},
+              {"Leaderboard", [](sf::RenderTarget& target) {}},
+              {"Settings", [](sf::RenderTarget& target) {}},
+              {"Exit", [&is_exit_requested](sf::RenderTarget& target) { is_exit_requested = true; }}};
+
+    game_menu::MenuConfig config{.title = "My Game", .items = items, .style = style};
+
+    auto                                                              menu_ptr = create_menu_context(w, config);
+    std::unique_ptr<game_menu::MENU, decltype(&menu_destroy_context)> menu(menu_ptr, &menu_destroy_context);
+    while (w.isOpen())
+    {
+        if (is_exit_requested)
+        {
+            w.close();
+            break;
+        }
+        sf::Event event;
+        while (w.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                is_exit_requested = true;
+            }
+            menu_handle_event(menu.get(), event);
+        }
+        w.clear();
+        menu_render(menu.get());
+        w.display();
+    }
+    return 0;
 }
-
--- public/game_menu/game_menu.h
+++ public/game_menu/game_menu.h
@@ -2,69 +2,81 @@
     Copyright (c) 2023 Sidhin S Thomas. All rights reserved
 */
 #include "SFML/Graphics/RenderTarget.hpp"
+
 #include <SFML/Graphics.hpp>
 
 #include <functional>
 #include <string>
 
-namespace game_menu {
+namespace game_menu
+{
 
 typedef struct __menu_context MENU;
 
 typedef uint32_t Color;
 
-enum Align { Left = 0, Center = 1, Right = 2 };
-
-struct MenuItem {
-  std::string name;
-  std::function<void(sf::RenderTarget &)> action;
+enum Align
+{
+    Left   = 0,
+    Center = 1,
+    Right  = 2
 };
 
-struct ColorScheme {
-  Color titleColor;
-  Color itemColor;
-  Color selectedColor;
+struct MenuItem
+{
+    std::string                            name;
+    std::function<void(sf::RenderTarget&)> action;
 };
 
-struct Padding {
-  int top;
-  int left;
-  int bottom;
-  int right;
+struct ColorScheme
+{
+    Color titleColor;
+    Color itemColor;
+    Color selectedColor;
 };
 
-struct Style {
-  sf::Font *TitleFont;
-  sf::Font *ItemFont;
-
-  uint32_t TitleFontSize;
-  uint32_t ItemFontSize;
-
-  float MenuTitleScaleFactor;
-  float MenuItemScaleFactor;
-
-  ColorScheme colorScheme;
-
-  Padding PaddingTitle;
-  Padding PaddingItems;
-
-  Align TitleAlign;
-  Align ItemAlign;
+struct Padding
+{
+    int top;
+    int left;
+    int bottom;
+    int right;
 };
 
-struct MenuConfig {
-  std::string title;
-  std::vector<MenuItem> items;
-  Style style;
+struct Style
+{
+    sf::Font* TitleFont;
+    sf::Font* ItemFont;
+
+    uint32_t TitleFontSize;
+    uint32_t ItemFontSize;
+
+    float MenuTitleScaleFactor;
+    float MenuItemScaleFactor;
+
+    ColorScheme colorScheme;
+
+    Padding PaddingTitle;
+    Padding PaddingItems;
+
+    Align TitleAlign;
+    Align ItemAlign;
+};
+
+struct MenuConfig
+{
+    std::string           title;
+    std::vector<MenuItem> items;
+    Style                 style;
 };
 
 } // namespace game_menu
 
-extern "C" {
+extern "C"
+{
 
-game_menu::MENU* create_menu_context(sf::RenderWindow &wnd, game_menu::MenuConfig &config);
-void menu_destroy_context(game_menu::MENU* menu);
-void menu_handle_event(game_menu::MENU* menu, sf::Event &event);
-void menu_render(game_menu::MENU* menu);
-
+    game_menu::MENU* create_menu_context(sf::RenderWindow& wnd, game_menu::MenuConfig& config);
+    void             menu_destroy_context(game_menu::MENU* menu);
+    void             menu_handle_event(game_menu::MENU* menu, sf::Event& event);
+    void             menu_render(game_menu::MENU* menu);
 }
